406f33f57395f7fc155bb57ac0d52c8550574c06
matheus-bandeira <matheusbandeira944@gmail.com>
2019-02-23 10:22:16 -0300

    Ajustes no endpoint /categoris/{id}

--- src/main/java/br/com/unicarioca/services/CategoriaService.java
+++ src/main/java/br/com/unicarioca/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.unicarioca.domain.Categoria;
 import br.com.unicarioca.repositories.CategoriaRepository;
+import br.com.unicarioca.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto Não encontrado! Id: " + id + ", Tipo " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
